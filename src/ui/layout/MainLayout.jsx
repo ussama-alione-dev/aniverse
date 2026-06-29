c94e931db0a6e97b853eef74eb9b209cb31ac680
alione10-lm <aliouaneoussama89@gmail.com>
2026-06-29 12:22:24 +0100

    feat: add Footer and MainLayout components; update App to include new layout structure

---
+++ src/ui/layout/MainLayout.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import { Outlet } from "react-router-dom";
+
+const MainLayout = () => {
+    return (
+        <div className="min-h-screen flex flex-col">
+            <Navbar />
+
+            <main className="flex-1">
+                <Outlet />
+            </main>
+
+            <Footer />
+        </div>
+    );
+};
+
+export default MainLayout;
