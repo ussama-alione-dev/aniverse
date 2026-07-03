0ff9baf15f316c152dfbcc52ed3e4c40e87f2626
alione10-lm <aliouaneoussama89@gmail.com>
2026-07-03 11:48:05 +0100

    feat: add favorite anime entry for Texhnolyze; update Home component layout and enhance ErrorMessage with retry button

--- src/ui/components/ErrorMessage.jsx
+++ src/ui/components/ErrorMessage.jsx
@@ -1,18 +1,27 @@
-import { BadgeAlert } from "lucide-react";
+import { BadgeAlert, RotateCw } from "lucide-react";
 import React from "react";
+import Button from "./Button";
 
 const ErrorMessage = ({ message }) => {
     return (
         <div className="w-fit h-96 flex items-center justify-center">
-            <p className="flex md:flex-row flex-col   items-start gap-4 text-muted-foreground/70 bg-card px-4 md:p-10 py-4  border ">
+            <div className="flex md:flex-row flex-col   items-start gap-4 text-muted-foreground/70 bg-card px-4 md:p-10 py-4  border ">
                 <BadgeAlert className="stroke-destructive" />
                 <div>
                     <p className="text-destructive">{message}</p>
                     <p className="text-sm text-muted-foreground/70">
                         please wait a few minutes and try again
                     </p>
+
+                    <Button
+                        onClick={() => window.location.reload()}
+                        variant="secondary"
+                        className="mt-4 flex items-center gap-2"
+                    >
+                        try again <RotateCw size={16} />
+                    </Button>
                 </div>
-            </p>
+            </div>
         </div>
     );
 };
